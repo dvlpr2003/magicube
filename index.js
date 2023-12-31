2b78a351ef3c923930d0172eac3e0a12328fb0ed
dvlpr_2003 <gayathrigaya698@gmail.com>
2023-12-31 07:47:50 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+var data = [
+  {
+    id: 1,
+    name: "raja",
+    age: 20,
+    address: "Thirumalaikeni",
+  },
+  {
+    id: 2,
+    name: "gayathri",
+    age: 20,
+    address: "Siluvathur",
+  },
+  {
+    id: 3,
+    name: "vinupriya",
+    age: 20,
+    address: "Kambiliampatti",
+  },
+  {
+    id: 4,
+    name: "prabavathy",
+    age: 18,
+    address: "Kambiliampatti",
+  },
+];
+function getBook() {
+  return data;
+}
+
+console.log();
